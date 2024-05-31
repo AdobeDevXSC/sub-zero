7ab0cc68491d4dff23fd2b1bb0cd65653157c0ad
KP Parrish <krparrish@adobe.com>
2024-05-31 13:50:45 -0600

    updated video teaser block

--- blocks/video-teaser/video-teaser.js
+++ blocks/video-teaser/video-teaser.js
@@ -1,14 +1,12 @@
 import { makeVideo } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {    
-	console.log('block children', block.children)
-
     const videoSrc = block.querySelector('div > a');
 
     if(videoSrc.href.includes(window.hlx.codeBasePath)) {
       videoSrc.href = videoSrc.text;
     }
 
-    makeVideo(block.querySelector('div'), videoSrc.href);
+    makeVideo(videoSrc.closest('div'), videoSrc.href);
     videoSrc.remove();
 }
