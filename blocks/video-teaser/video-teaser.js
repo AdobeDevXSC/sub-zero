8cda638e16da44f6b389761353bb59b94532cbdd
KP Parrish <krparrish@adobe.com>
2024-06-02 15:57:49 -0600

    video teaser block styles completed desktop & mobile

--- blocks/video-teaser/video-teaser.js
+++ blocks/video-teaser/video-teaser.js
@@ -2,6 +2,7 @@ import { makeVideo } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {    
     const videoSrc = block.querySelector('div > a');
+	const videoSrcWrapper = videoSrc.closest('div')
 
     if(videoSrc.href.includes(window.hlx.codeBasePath)) {
       videoSrc.href = videoSrc.text;
@@ -9,4 +10,6 @@ export default async function decorate(block) {
 
     makeVideo(videoSrc.closest('div'), videoSrc.href);
     videoSrc.remove();
+	videoSrcWrapper.classList.remove('button-container');
+	videoSrcWrapper.classList.add('video-container');
 }
