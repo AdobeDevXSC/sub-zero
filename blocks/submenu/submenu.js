d19aaca9d854dd896dc8ba0b5e829c79b442cf21
KP Parrish <krparrish@adobe.com>
2024-06-02 23:08:33 -0600

    cards section styles updated for desktop and mobile

--- blocks/submenu/submenu.js
+++ blocks/submenu/submenu.js
@@ -1,23 +1,5 @@
 export default async function decorate(block) {
 	const list = block.querySelector('ul');
 	list.id = "menu-list"
-	const listWrapper = list.closest('div')
-	console.log(list)
-
-
-
-	const getRect = listWrapper.getBoundingClientRect()
-	console.log(getRect)
-
-	// $('#menu-list').on("scroll", function(e) {
-	// 	horizontal = e.currentTarget.scrollLeft;
-	// 	console.log(horizontal, window.innerWidth);
-	// 	});
-
-	// if(list.width > window.innerWidth){
-	// 	list.className = "scroll-more"
-	// } else {
-	// 	list.className = "no-scroll"
-	// }
-	list.className = "scroll-more"
+	// TODO: calculate scroll positions to add/remove gradient based on if there are remaning elements or not
 }
